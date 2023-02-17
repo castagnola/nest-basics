71ab9845eeec3e4af000985d66253a8355210013
castagnola <castagnola92@gmail.com>
2023-02-17 11:47:03 -0500

    Implementing Guards

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,15 +4,24 @@ import {
   NestModule,
   RequestMethod,
 } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthGuard } from './guards/auth.guard';
 import { AuthenticationMiddleware } from './middleware/authentication.middleware';
 import { RequestService } from './request.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService, RequestService],
+  providers: [
+    AppService,
+    RequestService,
+    {
+      provide: APP_GUARD,
+      useClass: AuthGuard, //Global Auth guard
+    },
+  ],
 })
 export class AppModule implements NestModule {
   //Configure middleware in our app
